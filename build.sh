1be480183c86d89045dfbfa0f677ac897e748ace
drewbourne <drew@firstbourne.com>
2012-03-01 17:41:24 +1100

    fix build.sh

--- build.sh
+++ build.sh
@@ -1,16 +1,25 @@
+# ASX FlexUnit Runner
+mxmlc asx_test/src/asx_test.mxml \
+-output=asx_test/bin/asx_test.swf \
+-debug=true \
+-sp+=asx/src \
+-sp+=asx_test/src \
+-library-path+=asx_test/libs
+
 # ASX SpecRunner
-mxmlc specs/AsxSpecRunner.mxml \
- -output=bin/AsxSpecs.swf \
- -debug=true \
- -sp specs \
- -sp src \
- -sp ../spectacular-as3/src/ \
- -sp ../hamcrest-as3/hamcrest/src \
+# mxmlc specs/AsxSpecRunner.mxml \
+#  -output=bin/AsxSpecs.swf \
+#  -debug=true \
+#  -sp specs \
+#  -sp src \
+#  -sp ../spectacular-as3/src/ \
+#  -sp ../hamcrest-as3/hamcrest/src \
 
 # swc
 compc \
- -include-sources src \
- -output bin/asx.swc
+-sp+=asx/src \
+-include-sources asx/src \
+-output asx/bin/asx.swc
 
 # generate docs
 # asdoc \
